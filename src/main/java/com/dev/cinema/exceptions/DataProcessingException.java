36a3218479ae038c5c88e6d5e1275fadffca050a
Vadim Chaika <proxik777@gmail.com>
2020-02-04 14:10:42 +0200

    added MovieDao/Impl, custom exception

---
+++ src/main/java/com/dev/cinema/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.dev.cinema.exceptions;
+
+public class DataProcessingException extends Exception {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
