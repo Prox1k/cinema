d1773bb76f11167032f4b2c2bd602f2eddd6b7c9
Vadim Chaika <proxik777@gmail.com>
2020-02-06 12:43:11 +0200

    added User(Dao/Service), auth service & hashing w/ salt

---
+++ src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.exceptions.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.AuthenticationService;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private static UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (hashPassword.equals(user.getPassword())) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Incorrect email or password");
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        return userService.add(newUser);
+    }
+}
